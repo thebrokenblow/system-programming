8ddf91fff4f954ac55346f39e9a102ddc006803a
Артём Красов <artkrasov@gmail.com>
2026-01-28 19:18:25 +0300

    Сделал параллельное выполнение и асинхронное выполнение

---
+++ System Programming/TaskConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿var task1 = Task.Run(() =>
+{
+    int sum = 0;
+
+    for (int i = 1; i <= 1_000_000; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+});
+
+var task2 = Task.Run(() =>
+{
+    int dif = 0;
+
+    for (int i = 1; i <= 1_000_000; i++)
+    {
+        dif -= i;
+    }
+
+    return dif;
+});
+
+Task.WaitAll(task1, task2);
+
+Console.WriteLine(task1.Result);
+Console.WriteLine(task2.Result);
+
+Console.ReadLine();
---
+++ System Programming/TaskDesktop/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TaskDesktop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        using var streamReader = new StreamReader("C:\\Users\\artem\\OneDrive\\Рабочий стол\\numbers.txt");
+        var result = new List<string>();
+        while (true)
+        {
+            var strNumber = await streamReader.ReadLineAsync();
+
+            if (strNumber == null)
+            {
+                break;
+            }
+
+            var number = Convert.ToInt32(strNumber);
+
+            if (IsPrimeNumber(number))
+            {
+                result.Add(strNumber);
+            }
+        }
+
+        await File.WriteAllLinesAsync("C:\\Users\\artem\\OneDrive\\Рабочий стол\\result.txt", result);
+    }
+
+    private static bool IsPrimeNumber(int number)
+    {
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
